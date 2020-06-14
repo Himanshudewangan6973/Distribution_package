213e566b37f24c2c8a7b12346a8fb830a7d2c4e6
Himanshudewangan6973 <himanshudewangan6973@gmail.com>
2020-06-14 04:59:51 +0000

    Small corrections

--- 5_exercise_upload_to_pypi/setup.py
+++ 5_exercise_upload_to_pypi/setup.py
@@ -1,14 +1,7 @@
 from setuptools import setup
 
-<<<<<<< HEAD
 setup(name='stat_prob_distributions',
       version='0.1',
-      description='Gaussian distributions',
+      description='Gaussian  and Binomial distributions',
       packages=['stat_prob_distributions'],
-=======
-setup(name='distributions',
-      version='0.1',
-      description='Gaussian distributions',
-      packages=['distributions'],
->>>>>>> 42a2c1c176a4f1f73f586524675c4a74cb0e9c83
       zip_safe=False)
