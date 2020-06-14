ac6fdd9db2b3fc8e0d3d6f782b2dced4e1ceb200
Himanshudewangan6973 <himanshudewangan6973@gmail.com>
2020-06-14 04:33:11 +0000

    Finalize package 'stat_prob_distribution 0.1'

---
+++ 5_exercise_upload_to_pypi/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='stat_prob_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['stat_prob_distributions'],
+      zip_safe=False)
